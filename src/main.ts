fd43c1ccd9db78b9dcd4c8ac148ee970219b9a4f
김지호 <crsn1111@gmail.com>
2023-09-17 11:57:20 +0900

    origin true

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 유효성 검사 실패시 Validate에 도달하지않음
@@ -15,6 +11,10 @@ async function bootstrap() {
       transform: true, // path parameter에 대한 타입 추론이 가능해짐
     }),
   );
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
