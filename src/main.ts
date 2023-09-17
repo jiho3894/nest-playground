861aa2faa8eb63b254f6945aec51a43696c76b2b
김지호 <crsn1111@gmail.com>
2023-09-17 11:42:55 +0900

    cors setting

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 유효성 검사 실패시 Validate에 도달하지않음
